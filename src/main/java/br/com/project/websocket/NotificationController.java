d91306674e9c69d76bd253047856e3b0ef894875
Gerson Batista <gersonkenedy@hotmail.com>
2023-11-18 23:55:12 -0300

    add websocket

---
+++ src/main/java/br/com/project/websocket/NotificationController.java
@@ -0,0 +1,28 @@
+package br.com.project.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    // Initialize Notifications
+    private Notifications notifications = new Notifications(0);
+
+    @GetMapping("/notify")
+    public String getNotification() {
+
+        // Increment Notification by one
+        notifications.increment();
+
+        // Push notifications to front-end
+        template.convertAndSend("/topic/notification", notifications);
+
+        return "Notifications successfully sent to Angular !";
+    }
+}
---
+++ src/main/java/br/com/project/websocket/Notifications.java
@@ -0,0 +1,18 @@
+package br.com.project.websocket;
+public class Notifications {
+
+    private int count;
+
+    public Notifications(int count) {
+        this.count = count;
+    }
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public void increment() {
+        this.count++;
+    }
+}
